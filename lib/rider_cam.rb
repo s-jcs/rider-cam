613d5b4b6f5b54c69a3d58dfee0c5639178d8f59
s-jcs <shosundberg@gmail.com>
2021-12-12 23:52:55 +0900

    :wrench: upload all files in uploads

--- lib/rider_cam.rb
+++ lib/rider_cam.rb
@@ -1,18 +1,23 @@
 # frozen_string_literal: true
 
 require 'pry-byebug'
-require 'lib/rider_cam/drive'
+require './lib/rider_cam/drive.rb'
 
 module RiderCam
-  def upload_files(dir = 'tmp/uploads/')
-    drive = RiderCam::Drive.new
-    file = drive.service.create_file(
-      { name: 'photo.jpg' },
-      fields: 'id',
-      uplaod_source: dir,
-      content_type: 'image/jpeg'
-    )
 
-    puts "Uploaded: #{file.id}"
+  def self.upload_files(dir = './tmp/uploads/')
+    drive = RiderCam::Drive.new
+
+    Dir[File.join(dir, '*.png')].each do |file_path|
+      file = drive.service.create_file(
+        { name: "#{DateTime.now.to_s}.png" },
+        fields: 'id',
+        upload_source: file_path,
+        content_type: 'image/png'
+      )
+      
+      puts "uploaded: #{file.id}"
+      File.delete(file_path)
+    end
   end
 end
