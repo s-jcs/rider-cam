f002789d9cf741599ee01d4aab6b4d0cb546b57f
s-jcs <shosundberg@gmail.com>
2021-12-19 17:23:20 +0900

    :wrench: update install

--- lib/rider_cam.rb
+++ lib/rider_cam.rb
@@ -8,7 +8,7 @@ module RiderCam
     def capture_or_upload
       drive = RiderCam::Drive.new
       capturing = false
-      sleep(10)
+      sleep(5)
 
       # NOTE: run LED
       RPi::GPIO.set_numbering :board
@@ -18,16 +18,19 @@ module RiderCam
       
       loop do
         if drive.net_connected?
+          pwm.frequency = 10
           system('pkill raspivid')
           capturing = false
           drive.upload_files if drive.uploadable_files?
         else
           next if capturing
+          pwm.frequency = 1
           system("raspivid -n -b 250000 \
                  -fps 3 -t 0 -w 640 -h 480 \
                  -o ./tmp/uploads/#{DateTime.now.to_s}.mp4")
+          capturing = true
         end
-        sleep(60)
+        sleep(10)
       end
     end
   end
