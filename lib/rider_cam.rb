ec3307340bb3b89b4fbbc2a6d7481063e89d7f1e
s-jcs <shosundberg@gmail.com>
2021-12-22 01:13:49 +0900

    :wrench: change install back to ruby tar

--- lib/rider_cam.rb
+++ lib/rider_cam.rb
@@ -1,30 +1,29 @@
 # frozen_string_literal: true
 
 require './lib/rider_cam/drive.rb'
-# require 'rpi_gpio'
+require 'rpi_gpio'
 
 module RiderCam
   class << self
     def capture_or_upload
       drive = RiderCam::Drive.new
       capturing = false
-      sleep(5)
 
       # NOTE: run LED
-      #RPi::GPIO.set_numbering :board
-      #RPi::GPIO.setup 40, as: :output
-      #pwm = RPi::GPIO::PWM.new(40, 1)
-      #pwm.start 50
+      RPi::GPIO.set_numbering :board
+      RPi::GPIO.setup 40, as: :output
+      pwm = RPi::GPIO::PWM.new(40, 1)
+      pwm.start 1
       
       loop do
         if drive.net_connected?
-          #pwm.frequency = 10
+          pwm.frequency = 3
           system('pkill raspivid')
           capturing = false
           drive.upload_files if drive.uploadable_files?
         else
           next if capturing
-          #pwm.frequency = 1
+          pwm.frequency = 1
           system("raspivid -n -b 250000 \
                  -fps 3 -t 0 -w 640 -h 480 \
                  -o ./tmp/uploads/#{DateTime.now.to_s}.mp4")
